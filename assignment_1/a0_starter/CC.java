cafc8213e7d06671cdb2b7be77454aa16c9799fe
sh6khan <sadman.khan999@gmail.com>
2017-05-11 17:37:41 -0400

    using hashsets

--- assignment_1/a0_starter/CC.java
+++ assignment_1/a0_starter/CC.java
@@ -21,37 +21,32 @@ public class CC {
         int maxVertex = 0;
 
         int numthreads = 4;
-        HashMap<Integer, Integer> map = new HashMap<>();
         Path path = Paths.get(args[1]);
         data = Files.readAllBytes(path);
+        int edgeCount = 0;
+        for (int i = 0; i < data.length; i++){
+            if (data[i] == NEWLINE) {
+                edgeCount++;
+            }
+        }
+
+        // there can be a max of 2 * the number of edges
+        int nodeCount = edgeCount * 2;
+        int[] allNodes = new int[nodeCount];
+        int index = 0;
+
+
         int number_1 = 0;
         int number = 0;
-        int max = 0;
-        for (int i = 0; i < data.length; i++){
-            if (data[i] == SPACE || data[i] == NEWLINE) {
-                if (number > max){
-                    max = number;
-               }
-               number = 0;
-            }
-            else {
-                number = number * 10 + (data[i] - 48);
-
-            }
-        }
-
-        int[] biject = new int[max+1];
-        UnionFind uf = new UnionFind(max + 1);
-
-
          for (int i = 0; i < data.length; i++){
-            if (data[i] == SPACE) { 
+            if (data[i] == SPACE) {
                 number_1 = number;
-                number = 0;                
+                number = 0;
             }
             else if (data[i] == NEWLINE) {
-                uf.union(number_1, number);
-                number = 0;  
+                allNodes[index++] = number_1;
+                allNodes[index++] = number;
+                number = 0;
             }
             else {
                 number = number * 10 + (data[i] - 48);
@@ -59,8 +54,16 @@ public class CC {
             }
         }
 
+        UnionFind uf = new UnionFind(allNodes);
+
+
+        for (int i = 0; i < allNodes.length; i++) {
+            uf.union(allNodes[i++], allNodes[i]);
+        }
+
+
         try {
-            uf.printToFile(args[2]);
+            uf.printToFile(args[2], allNodes);
         } catch (Exception e) {
             System.out.println("failed to write to file");
              // do nothing;
@@ -73,30 +76,50 @@ public class CC {
      */
     static public class UnionFind {
 
-        private int[] componenentSizeMap;
-        public int[] mapping;
-        private boolean[] exists;
+        private int[] _componenentSizeMap;
+        public int[] _mappings;
+        public int[] _reverse_bijection;
+        private HashMap<Integer, Integer> _bijection = new HashMap<>();
 
-        public UnionFind(int maxNode) {
-            mapping = new int[maxNode];
-            exists = new boolean[maxNode];
-            componenentSizeMap = new int[maxNode];
 
-            for (int i = 0; i < maxNode; i++) {
-                mapping[i] = i;
-                componenentSizeMap[i] = 1;
+
+        public UnionFind(int[] allNodes) {
+            int id = 0;
+            _reverse_bijection = new int[allNodes.length];
+
+            for (int node : allNodes) {
+                if (!_bijection.containsKey(node)) {
+                    _bijection.put(node, id);
+                    _reverse_bijection[id] = node;
+                    id++;
+                }
             }
+
+            int size = _bijection.size();
+
+            _mappings = new int[size];
+            _componenentSizeMap = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                _mappings[i] = i;
+                _componenentSizeMap[i] = 1;
+            }
+
         }
 
         public int find(int node) {
             // Find the root of the component
             // which is defined when root == mapping[root]
             int root = node;
-            while( root != mapping[root] ) {
-                root = mapping[root];
+            while( root != _mappings[root] ) {
+                root = _mappings[root];
             }
 
-            mapping[node] = root;
+            while(node != root) {
+                int next = _mappings[node];
+                _mappings[node] = root;
+                node = next;
+            }
 
             return root;
         }
@@ -104,35 +127,32 @@ public class CC {
         /**
          * Connect the root nodes of two seperate components
          */
-        public void union(int nodeA, int nodeB) {
+        public void union(int a, int b) {
+            int nodeA = _bijection.get(a);
+            int nodeB = _bijection.get(b);
+
             int nodeARoot = find(nodeA);
             int nodeBRoot = find(nodeB);
 
             if (nodeARoot == nodeBRoot) {
                 return;
             }
+            
 
-            if (componenentSizeMap[nodeARoot] < componenentSizeMap[nodeBRoot]) {
-                mapping[nodeARoot] = nodeBRoot;
-                componenentSizeMap[nodeBRoot] += componenentSizeMap[nodeARoot];
+            if (_componenentSizeMap[nodeARoot] < _componenentSizeMap[nodeBRoot]) {
+                _mappings[nodeARoot] = nodeBRoot;
+                _componenentSizeMap[nodeBRoot] += _componenentSizeMap[nodeARoot];
             } else {
-                mapping[nodeBRoot] = nodeARoot;
-                componenentSizeMap[nodeARoot] += componenentSizeMap[nodeBRoot];
+                _mappings[nodeBRoot] = nodeARoot;
+                _componenentSizeMap[nodeARoot] += _componenentSizeMap[nodeBRoot];
             }
 
-
-            exists[nodeA] = true;
-            exists[nodeB] = true;
         }
 
-        public void printToFile(String fileName) throws IOException {
+        public void printToFile(String fileName, int[] allNodes) throws IOException {
             PrintWriter pw = new PrintWriter(new FileWriter(fileName));
-            for (int i = 0; i < mapping.length; i++) {
-                if (exists[i] != true) {
-                    continue;
-                }
-
-                pw.println(i + " " + find(i));
+            for (int i : _bijection.keySet()) {
+                pw.println(i + " " + _reverse_bijection[find(_bijection.get(i))]);
             }
 
             pw.close();
