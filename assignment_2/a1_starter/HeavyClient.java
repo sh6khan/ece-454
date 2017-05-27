b5788ff0e594716f227dda08396e89426f6c4f02
Guozhen La <guozhen.la@gmail.com>
2017-05-27 19:50:38 -0400

    heavy client

---
+++ assignment_2/a1_starter/HeavyClient.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TTransportFactory;
+
+public class HeavyClient {
+    public static void main(String [] args) {
+        try {
+            String plaintextPassword = "ABCDEFG";
+
+            TSocket sock = new TSocket(args[0], Integer.parseInt(args[1]));
+            TTransport transport = new TFramedTransport(sock);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            BcryptService.Client client = new BcryptService.Client(protocol);
+            transport.open();
+
+            List<String> password = genPasswordList();
+            List<String> hash = client.hashPassword(password, (short)10);
+            System.out.println("Check: " + client.checkPassword(password, hash));
+
+            transport.close();
+        } catch (TException x) {
+            x.printStackTrace();
+        }
+    }
+
+    public static List<String> genPasswordList() {
+        List<String> l = new ArrayList<String>(1024);
+        String somebigpassword = "faldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurewqodfnmdsalkfjdsalkfjaslkfajflasdjfadslfkajdflkjfdalkadfjlkdfjfadsflkjafaldskfjalkdsjfalkfdjasfoeiurqoeueoirqueroqiewurvcvmvcmdoiZZ";
+        for (int i = 0; i < 100; i++) {
+            l.add(somebigpassword + i);
+        }
+        return l;
+    }
+}
