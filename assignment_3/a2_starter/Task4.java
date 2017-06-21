7822b861f4b70566bb0c0ef0e025040da287e4fd
sh6khan <sadman.khan999@gmail.com>
2017-06-20 22:50:50 -0400

    task 4 starting

--- assignment_3/a2_starter/Task4.java
+++ assignment_3/a2_starter/Task4.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -13,6 +14,81 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task4 {
-  public static void main(String[] args) throws Exception {
+  public static class ParsingMapper extends Mapper<Object, Text, Text, ArrayPrimitiveWritable> {
+    private Text movieTitle = new Text();
+    private final static IntWritable rating = new IntWritable();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] movies = value.toString().split("\n");
+
+
+      StringBuilder sb = new StringBuilder();
+      for (String movie : movies) {
+        String[] tokens = movie.split(",");
+        int[] ratings = new int[tokens.length - 1];
+
+        movieTitle.set(tokens[0]);
+
+        int val = 0;
+        for (int i = 1; i < tokens.length; i++) {
+          String token = tokens[i];
+
+          if (token.equals("")) {
+            val = 0;
+          } else {
+            val = Integer.valueOf(token);
+          }
+
+          ratings[i] = val;
+        }
+
+        context.write(movieTitle, new ArrayPrimitiveWritable(ratings));
+      }
+    }
   }
+
+
+  public static class CartesionReducer extends Reducer<Text, ArrayPrimitiveWritable, Text, Text> {
+    private static HashMap<String, int[]> inMemory = new HashMap<>();
+
+    public void reduce(Text key, ArrayPrimitiveWritable values, Context context) throws IOException, InterruptedException {
+      String movieTitle = key.toString();
+
+      int[] ratings = (int[]) values.get();
+
+//      for (Map.Entry<String, int[]> entry : inMemory.entrySet()) {
+//
+//      }
+
+      inMemory.put(movieTitle, ratings);
+      System.out.println("Map size: " + inMemory.size());
+
+    }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapred.textoutputformat.separator", ",");
+
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: rating avg <in> <out>");
+      System.exit(2);
+    }
+
+
+    Job job = new Job(conf, "cartesion product calc");
+    job.setJarByClass(Task4.class);
+    job.setMapperClass(Task4.ParsingMapper.class);
+    job.setReducerClass(Task4.CartesionReducer.class);
+    job.setNumReduceTasks(1);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+
 }
