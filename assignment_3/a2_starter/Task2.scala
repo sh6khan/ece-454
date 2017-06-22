6b4709f876b91edaf1f4377c213d240057c99c41
Guozhen La <guozhen.la@gmail.com>
2017-06-22 18:23:03 -0400

    task 2

--- assignment_3/a2_starter/Task2.scala
+++ assignment_3/a2_starter/Task2.scala
@@ -1,6 +1,18 @@
 import org.apache.spark.{SparkContext, SparkConf}
 
 object Task2 {
+  def countNonEmpty(s: String): Int = {
+    var count = 0
+    val scores = s.split(",").drop(0)
+
+    for (i <- 1 to scores.length - 1) {
+      if (scores(i).nonEmpty) {
+        count = count + 1
+      }
+    }
+    count
+  }
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 2")
     val sc = new SparkContext(conf)
@@ -8,8 +20,8 @@ object Task2 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
-    output.saveAsTextFile(args(1))
+    val output = textFile.map(line => countNonEmpty(line)).reduce(_ + _)
+    val out1 = output.collect()
+    out1.saveAsTextFile(args(1))
   }
 }
---
+++ assignment_3/a2_starter/a2_task1_spark_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/java/jdk1.7.0_67-cloudera
+export SCALA_HOME=/usr
+export CDH_HOME=/opt/cloudera/parcels/CDH-5.11.0-1.cdh5.11.0.p0.34/
+
+echo --- Deleting
+rm Task1.jar
+rm Task1*.class
+
+echo --- Compiling
+$SCALA_HOME/bin/scalac -J-Xmx1g Task1.scala
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+echo --- Jarring
+$JAVA_HOME/bin/jar -cf Task1.jar Task1*.class
+
+echo --- Running
+INPUT=/tmp/in0.txt
+OUTPUT=/user/${USER}/a1_starter_code_output/
+
+hdfs dfs -rm -R $OUTPUT
+time $SPARK_HOME/bin/spark-submit --class Task1 Task1.jar $INPUT $OUTPUT
+
+hdfs dfs -ls $OUTPUT
