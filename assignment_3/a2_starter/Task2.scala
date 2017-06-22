3eb574017be6e0a8046b21a8508822c815ea5a89
Guozhen La <guozhen.la@gmail.com>
2017-06-22 18:40:09 -0400

    task 2

--- assignment_3/a2_starter/Task2.scala
+++ assignment_3/a2_starter/Task2.scala
@@ -1,17 +1,6 @@
 import org.apache.spark.{SparkContext, SparkConf}
 
 object Task2 {
-  def countNonEmpty(s: String): Int = {
-    var count = 0
-    val scores = s.split(",").drop(0)
-
-    for (i <- 1 to scores.length - 1) {
-      if (scores(i).nonEmpty) {
-        count = count + 1
-      }
-    }
-    count
-  }
 
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 2")
@@ -20,8 +9,7 @@ object Task2 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(line => countNonEmpty(line)).reduce(_ + _)
-    val out1 = output.collect()
-    out1.saveAsTextFile(args(1))
-  }
+    val output = textFile.flatMap(line => line.split(",").drop(0).filter(_.nonEmpty))
+      .count
+    sc.parallelize(Seq(output)).coalesce(1).saveAsTextFile(args(1))}
 }
