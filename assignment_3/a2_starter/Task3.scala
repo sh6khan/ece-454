68250121a3fccbd76019ba75ea093365c32c21d3
Guozhen La <guozhen.la@gmail.com>
2017-06-22 21:09:42 -0400

    task 4

--- assignment_3/a2_starter/Task3.scala
+++ assignment_3/a2_starter/Task3.scala
@@ -1,6 +1,15 @@
 import org.apache.spark.{SparkContext, SparkConf}
 
 object Task3 {
+  def mapping(line: String): Unit = {
+    val arr = line.split(",")
+
+    for (i <- 1 to arr.length - 1) {
+
+    }
+
+
+  }
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 3")
     val sc = new SparkContext(conf)
@@ -8,8 +17,8 @@ object Task3 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
+    val output = textFile.flatMap(line => mapping(line))
+
     output.saveAsTextFile(args(1))
   }
 }
--- assignment_3/a2_starter/Task4.scala
+++ assignment_3/a2_starter/Task4.scala
@@ -1,6 +1,31 @@
 import org.apache.spark.{SparkContext, SparkConf}
 
 object Task4 {
+  def calc(mv1: String, mv2: String): String = {
+    if (mv1 == mv2 || mv1.compareTo(mv2) >= 0) {
+      return ""
+    } else {
+      var arr1 = mv1.split(",")
+      var arr2 = mv2.split(",")
+
+      var ret = mv1(0) + "," + mv2(0)
+
+      arr1 = arr1.drop(1).map(a => if (a.nonEmpty) a.toInt else 0)
+      arr2 = arr2.drop(1).map(b => if (b.nonEmpty) b.toInt else 0)
+
+
+      val mag1 = Math.sqrt(arr1.map(x => Math.pow(x, 2)).sum)
+      val mag2 = Math.sqrt(arr2.map(y => Math.pow(y, 2)).sum)
+
+      var dot = 0
+      for (i <- 0 to arr1.length -1){
+        dot = dot + arr1(i) * arr2(i)
+      }
+      cosine = dot / (mag1 * mag2)
+      ret = ret + "," + f"$cosine%1.2f"
+    }
+  }
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 4")
     val sc = new SparkContext(conf)
@@ -8,8 +33,8 @@ object Task4 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
+    val output = textFile.cartesian(textFile).map(pair => calc(pair._1, pair._2)).filter(_.nonEmpty)
+
     output.saveAsTextFile(args(1))
   }
 }
