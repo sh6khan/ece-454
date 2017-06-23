980d2d3cdafe313a1066876ce281d9c9f0584965
Guozhen La <guozhen.la@gmail.com>
2017-06-22 22:02:18 -0400

    filter 1

--- assignment_3/a2_starter/Task4.scala
+++ assignment_3/a2_starter/Task4.scala
@@ -2,18 +2,22 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object Task4 {
   def calc(arr1: Array[String], arr2: Array[String]): String = {
-      val arr1i = arr1.drop(1).map(a => if (a.nonEmpty) a.toInt else 0)
-      val arr2i = arr2.drop(1).map(b => if (b.nonEmpty) b.toInt else 0)
+      if (arr1(0).compareTo(arr2(0)) >= 0) {
+        return ""
+      } else {
+        val arr1i = arr1.drop(1).map(a => if (a.nonEmpty) a.toInt else 0)
+        val arr2i = arr2.drop(1).map(b => if (b.nonEmpty) b.toInt else 0)
 
-      val mag1 = Math.sqrt(arr1i.map(x => Math.pow(x, 2)).sum)
-      val mag2 = Math.sqrt(arr2i.map(y => Math.pow(y, 2)).sum)
+        val mag1 = Math.sqrt(arr1i.map(x => Math.pow(x, 2)).sum)
+        val mag2 = Math.sqrt(arr2i.map(y => Math.pow(y, 2)).sum)
 
-      var dot = 0
-      for (i <- 0 to arr1i.length -1){
-        dot = dot + arr1i(i) * arr2i(i)
+        var dot = 0
+        for (i <- 0 to arr1i.length -1){
+          dot = dot + arr1i(i) * arr2i(i)
+        }
+        val cosine = dot / (mag1 * mag2)
+        arr1(0) + "," + arr2(0) + "," + f"$cosine%1.2f"
       }
-      val cosine = dot / (mag1 * mag2)
-      arr1(0) + "," + arr2(0) + "," + f"$cosine%1.2f"
   }
 
   def main(args: Array[String]) {
@@ -25,8 +29,8 @@ object Task4 {
     // modify this code
     val output = textFile.map(line => line.split(","))
       .cartesian(textFile)
-      .filter(_._1(0).compareTo(_._2(0)) < 0)
       .map(pair => calc(pair._1, pair._2))
+      .filter(_.nonEmpty)
 
     output.saveAsTextFile(args(1))
   }
