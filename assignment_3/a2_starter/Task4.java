53e53b22d5c93db0a3e32cc813ac7fb9ba6d81af
sh6khan <sadman.khan999@gmail.com>
2017-06-22 22:11:06 -0400

    task 4 with mapside join

--- assignment_3/a2_starter/Task4.java
+++ assignment_3/a2_starter/Task4.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,6 +10,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.ArrayPrimitiveWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,68 +20,60 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task4 {
-  public static class ParsingMapper extends Mapper<Object, Text, Text, ArrayPrimitiveWritable> {
-    private Text movieTitle = new Text();
-    private final static IntWritable rating = new IntWritable();
+  public static class ParsingMapper extends Mapper<Object, Text, NullWritable, Text> {
+    private Text result = new Text();
+    private static HashMap<String, int[]> allData = new HashMap<>();
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException{
+      Configuration conf = context.getConfiguration();
+      URI[] inputURI = Job.getInstance(conf).getCacheFiles();
 
-      String[] tokens = value.toString().split(",", -1);
-      int[] ratings = new int[tokens.length - 1];
+      for (URI uri : inputURI) {
+        Path path = new Path(uri.getPath());
+        String filePathName = path.getName();
+        BufferedReader reader = new BufferedReader(new FileReader(filePathName));
 
-      movieTitle.set(tokens[0]);
+        String line = "";
+        while((line = reader.readLine()) != null) {
+          String[] tokens = line.split(",", -1);
+          int[] ratings = new int[tokens.length - 1];
 
-      int val = 0;
-      for (int i = 1; i < tokens.length; i++) {
-        String token = tokens[i];
+          int val = 0;
+          for (int i = 1; i < tokens.length; i++) {
+            String token = tokens[i];
+            val = token.equals("") ? 0 : Integer.valueOf(token);
+            ratings[i - 1] = val;
+          }
 
-        if (token.equals("")) {
-          val = 0;
-        } else {
-          val = Integer.valueOf(token);
+          allData.put(tokens[0], ratings);
+
         }
-
-        ratings[i - 1] = val;
       }
-
-      context.write(movieTitle, new ArrayPrimitiveWritable(ratings));
     }
 
-  }
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] movieTitle = (String[]) allData.keySet().toArray();
+      int[] otherRating;
+      int[] rating;
+      double cosine;
+      String movieName;
+      for (int i = 0; i < movieTitle.length; i++) {
+        for (int j = i + 1; j < movieTitle.length; j++) {
+          otherRating = allData.get(movieTitle[i]);
+          rating = allData.get(movieTitle[j]);
+          cosine = calculate(rating, otherRating);
 
+          if (movieTitle[i].compareTo(movieTitle[j]) < 0) {
+            movieName = movieTitle[i] + "," + movieTitle[j];
+          } else {
+            movieName = movieTitle[j] + "," + movieTitle[i];
+          }
 
-  public static class CartesionReducer extends Reducer<Text, ArrayPrimitiveWritable, Text, Text> {
-    private static HashMap<String, int[]> inMemory = new HashMap<>();
-    private static Text moviePair = new Text();
-    private static Text ratingCosine = new Text();
-
-
-    public void reduce(Text key, Iterable<ArrayPrimitiveWritable> values, Context context) throws IOException, InterruptedException {
-      String movieTitle = key.toString();
-      System.out.println(movieTitle);
-
-      int[] ratings = (int[]) values.iterator().next().get();
-
-      StringBuilder sb;
-      for (Map.Entry<String, int[]> entry : inMemory.entrySet()) {
-        sb = new StringBuilder();
-        System.out.println(entry.getKey());
-
-        if (movieTitle.compareTo(entry.getKey()) < 0) {
-          sb.append(movieTitle).append(",").append(entry.getKey());
-        } else {
-          sb.append(entry.getKey()).append(",").append(movieTitle);
+          result.set(movieName + String.format("%1.2f", cosine));
+          context.write(NullWritable.get(), result);
         }
-
-        double res = calculate(ratings, entry.getValue());
-
-        moviePair.set(sb.toString());
-        ratingCosine.set(String.format("%1.2f", res));
-
-        context.write(moviePair, ratingCosine);
       }
-      
-      inMemory.put(movieTitle, ratings);
     }
 
     public double calculate(int[] a, int[] b) {
@@ -96,6 +93,42 @@ public class Task4 {
   }
 
 
+//  public static class CartesionReducer extends Reducer<Text, ArrayPrimitiveWritable, Text, Text> {
+//    private static HashMap<String, int[]> inMemory = new HashMap<>();
+//    private static Text moviePair = new Text();
+//    private static Text ratingCosine = new Text();
+//
+//
+//    public void reduce(Text key, Iterable<ArrayPrimitiveWritable> values, Context context) throws IOException, InterruptedException {
+//      String movieTitle = key.toString();
+//      System.out.println(movieTitle);
+//
+//      int[] ratings = (int[]) values.iterator().next().get();
+//
+//      StringBuilder sb;
+//      for (Map.Entry<String, int[]> entry : inMemory.entrySet()) {
+//        sb = new StringBuilder();
+//        System.out.println(entry.getKey());
+//
+//        if (movieTitle.compareTo(entry.getKey()) < 0) {
+//          sb.append(movieTitle).append(",").append(entry.getKey());
+//        } else {
+//          sb.append(entry.getKey()).append(",").append(movieTitle);
+//        }
+//
+//        double res = calculate(ratings, entry.getValue());
+//
+//        moviePair.set(sb.toString());
+//        ratingCosine.set(String.format("%1.2f", res));
+//
+//        context.write(moviePair, ratingCosine);
+//      }
+//
+//      inMemory.put(movieTitle, ratings);
+//    }
+//  }
+
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     conf.set("mapred.textoutputformat.separator", ",");
@@ -109,10 +142,24 @@ public class Task4 {
     Job job = new Job(conf, "cartesion product calc");
     job.setJarByClass(Task4.class);
     job.setMapperClass(Task4.ParsingMapper.class);
-    job.setReducerClass(Task4.CartesionReducer.class);
-    job.setNumReduceTasks(1);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(ArrayPrimitiveWritable.class);
+    // job.setReducerClass(Task4.CartesionReducer.class);
+
+    // add all the input files into the hadoop distributed cache
+    File fp = new File(otherArgs[0]);
+    // if its a directory then add all subfiles
+    if (fp.isDirectory()) {
+      File[] allFiles = fp.listFiles();
+      for (File file : allFiles) {
+        job.addCacheFile(new Path(file.getAbsolutePath()).toUri());
+      }
+    } else {
+      // else just add this one file
+      job.addCacheFile(new Path(otherArgs[0]).toUri());
+    }
+
+    job.setOutputKeyClass(NullWritable.class);
+    job.setOutputValueClass(Text.class);
+    job.setNumReduceTasks(0);
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
