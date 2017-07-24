eac163da6ede69f968974b3a090b1148c1f62007
sh6khan <sadman.khan999@gmail.com>
2017-07-23 22:03:56 -0400

    logging all the good shit

--- assignment_5/a4_starter/A4ServiceHandler.java
+++ assignment_5/a4_starter/A4ServiceHandler.java
@@ -3,6 +3,10 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+
 
 import io.atomix.copycat.client.CopycatClient;
 
@@ -13,12 +17,17 @@ public class A4ServiceHandler implements A4Service.Iface {
     private String ccHost;
     private int ccPort;
 
+    static Logger log;
+
 	private final ExecutorService service = Executors.newSingleThreadExecutor();
 
     public A4ServiceHandler(String ccHost, int ccPort) {
 		this.ccHost = ccHost;
 		this.ccPort = ccPort;
 		clientMap = new ConcurrentHashMap<>();
+
+		BasicConfigurator.configure();
+		log = Logger.getLogger(A4ServiceHandler.class.getName());
 
 		// start thread to commit CommandBuffer every 10ms
 		service.execute(new BatchTicker(ccHost, ccPort));
@@ -38,8 +47,9 @@ public class A4ServiceHandler implements A4Service.Iface {
 
 		CopycatClient client = getCopycatClient();
     	CommandBuffer.addIncrementCommand(key);
-		Long ret = client.submit(new GetQuery(key)).join();
-		return ret + CommandBuffer.getDelta(key);
+		Long ret = client.submit(new GetQuery(key)).join() + CommandBuffer.getDelta(key);
+		log.info("FAI called: " + key + ":" + ret);
+		return ret;
 
 		// improve this part
 //		synchronized (this) {
@@ -53,8 +63,9 @@ public class A4ServiceHandler implements A4Service.Iface {
 
 		CopycatClient client = getCopycatClient();
 		CommandBuffer.addDecrementCommand(key);
-		Long ret = client.submit(new GetQuery(key)).join();
-		return ret + CommandBuffer.getDelta(key);
+		Long ret = client.submit(new GetQuery(key)).join() + CommandBuffer.getDelta(key);
+		log.info("FAD called: " + key + ":" + ret);
+		return ret;
 
 //		// improve this part
 //		synchronized (this) {
@@ -67,6 +78,7 @@ public class A4ServiceHandler implements A4Service.Iface {
     public long get(String key) throws org.apache.thrift.TException {
 		CopycatClient client = getCopycatClient();
 		Long ret = client.submit(new GetQuery(key)).join();
+		log.info("get called: " + key + ":" + ret);
 		return ret;
     }
 }
